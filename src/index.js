23ab32510a8e88f374ae092d8c2baadae3d13bf6
TISLAP51 <rrajasekar@thapovan-inc.com>
2020-02-17 12:27:47 +0530

    Show tags only if atleast one API is using the tag

--- src/index.js
+++ src/index.js
@@ -23,17 +23,34 @@ async function run(){
   let paths = {};
   let tags = [];
   let tagNames = [];
+  let tagsInPaths = [];
+  for(let file in retobj){
+    let fileObj =  retobj[file];
+    let paths = fileObj.paths;
+    for(let pathIdx in paths){
+      let path = paths[pathIdx];
+      for(let method in path){
+        let methodTags = path[method].tags;
+        if(!methodTags instanceof Array){
+          methodTags = [methodTags];
+        }
+        tagsInPaths = tagsInPaths.concat(methodTags);
+      }
+    }
+  }
+  tagsInPaths = [...new Set(tagsInPaths)];
   for(let file in retobj){
     let fileObj = retobj[file];
     Object.assign(paths, fileObj.paths);
     for(let tagId in fileObj.tags){
       let tag = fileObj.tags[tagId];
-      if(!tagNames.includes(tag.name)){
+      if(!tagNames.includes(tag.name) && tagsInPaths.indexOf(tag.name) > -1){
         tagNames.push(tag.name);
         tags.push(tag);
       }
     }
   }
+  console.log('Tags in path', tagsInPaths);
   swaggerJson.tags = tags;
   swaggerJson.paths = paths;
   swaggerJson.definitions = defBuilder.getDefs();
